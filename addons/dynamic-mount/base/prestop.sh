965bfea43909effe03d409c63e3c68b28666712c
trafalgarzzz <trafalgarz@outlook.com>
2024-03-28 17:43:51 +0800

    Fix cleaning up sub directories
    Signed-off-by: trafalgarzzz <trafalgarz@outlook.com>

--- addons/dynamic-mount/base/prestop.sh
+++ addons/dynamic-mount/base/prestop.sh
@@ -14,7 +14,7 @@ set +e
 echo "prestop.sh: clean sub directories under ${MOUNT_POINT}"
 sub_dirs=$(ls "${MOUNT_POINT}/")
 for sub_dir in ${sub_dirs}; do
-    rmdir "${sub_dir}" || echo "WARNING: failed to rmdir ${sub_dir}, maybe filesystem still mounting on it."
+    rmdir "${MOUNT_POINT}/${sub_dir}" || echo "WARNING: failed to rmdir ${sub_dir}, maybe filesystem still mounting on it."
 done
 
 exit 0
